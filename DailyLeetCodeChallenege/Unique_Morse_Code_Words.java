1aaa01eadc948bb7b02f93813386c2c242096de3
Nitish Kumar <68236771+nitisharav@users.noreply.github.com>
2022-08-17 13:22:00 +0530

    Create Unique_Morse_Code_Words.java
    First Commit

---
+++ DailyLeetCodeChallenege/Unique_Morse_Code_Words.java
@@ -0,0 +1,10 @@
+//Unique Morse Code Words
+//Link= https://leetcode.com/problems/unique-morse-code-words/
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String pattern  []={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+       System.out.println('a'-97);
+        
+        return 2;
+    }
+}
