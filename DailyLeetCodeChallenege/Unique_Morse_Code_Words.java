4d94cb3d299907257ec2b55719b784e7e4e928ae
Nitish Kumar <68236771+nitisharav@users.noreply.github.com>
2022-08-17 14:21:31 +0530

    Update Unique_Morse_Code_Words.java
    2nd commit

--- DailyLeetCodeChallenege/Unique_Morse_Code_Words.java
+++ DailyLeetCodeChallenege/Unique_Morse_Code_Words.java
@@ -5,6 +5,22 @@ class Solution {
         String pattern  []={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
        System.out.println('a'-97);
         
-        return 2;
+        for (int i=0;i<words.length;i++){
+            char ch[]= words[i].toCharArray();
+            String s="";
+            for (char c:ch){
+                int x=c-97;
+                s=s+pattern[x];
+            }
+            words[i]=s;
+        }
+        Set<String> unq=new HashSet<>(Arrays.asList(words));
+        
+        return unq.size();
     }
 }
+
+//Learning
+// 1. From Array to hashSet in one line
+  //  Set<String> unq=new HashSet<>(Arrays.asList(words));
+   
