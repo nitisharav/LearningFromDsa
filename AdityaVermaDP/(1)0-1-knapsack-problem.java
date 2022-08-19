1c6991745a78b891f83c76729863e6e505d8bcaa
Nitish Kumar <68236771+nitisharav@users.noreply.github.com>
2022-08-19 21:50:29 +0530

    Create (1)0-1-knapsack-problem.java
    first commit

---
+++ AdityaVermaDP/(1)0-1-knapsack-problem.java
@@ -0,0 +1,30 @@
+//problem 1
+//Link= https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+     static int max(int a, int b)
+    {
+      return (a > b) ? a : b;
+    }
+    
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here
+         if (n==0|| W==0) return 0;
+         
+         if (wt[n-1]<=W)
+         return max(val[n-1]+knapSack(W-wt[n-1],wt,val,n-1), knapSack(W,wt,val,n-1) );
+         
+         else if(wt[n-1]>W) return knapSack(W,wt,val,n-1);
+         
+         return 0;
+    } 
+}
+
+
+//This solution giving TLE for 1163/1210 test case
+//Learning
+//step1 write base case by considering smallest valid input, and return value at min val
+//step 2 write recursion using choice diagram
+//step 3 start from end of array for writing recursion
