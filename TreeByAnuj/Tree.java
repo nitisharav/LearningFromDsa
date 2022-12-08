f5b05bfbf1850354fe5f7b8d6bdaa9d002bbe7cc
Nitish Kumar <68236771+nitisharav@users.noreply.github.com>
2022-12-09 01:42:49 +0530

    Update Tree.java

--- TreeByAnuj/Tree.java
+++ TreeByAnuj/Tree.java
@@ -8,7 +8,20 @@ public class Tree {
 	public static void main(String[] args) {
 		sc=new Scanner(System.in);
 		
-		createTree();
+		Node root= createTree();
+	    inOrder(root);
+	    System.out.println();
+	    preOrder(root);
+	    System.out.println();
+	    postOrder(root);
+	    System.out.println();
+	    System.out.println("height: "+height(root));
+	    System.out.println();
+	    System.out.println("size: "+ size(root));
+	    System.out.println();
+	    System.out.println("maximum: "+ maximum(root));
+	    System.out.println();
+	    System.out.println("minimum: "+ minimum(root));
 
 	}
 	
@@ -27,6 +40,43 @@ public class Tree {
 		root.right= createTree();
 		
 		return root;
+	}
+	
+	static void inOrder(Node root) {
+		if (root==null) return ;
+		inOrder(root.left);
+		System.out.print(root.data+ " ");
+		inOrder(root.right);
+	}
+	static void preOrder(Node root) {
+		if (root==null) return ;
+		System.out.print(root.data+ " ");
+		preOrder(root.left);
+		
+		preOrder(root.right);
+	}
+	static void postOrder(Node root) {
+		if (root==null) return ;
+		postOrder(root.left);
+		
+		postOrder(root.right);
+		System.out.print(root.data+ " ");
+	}
+	static int height(Node root) {
+		if (root==null) return 0;
+		return Math.max(height(root.left), height(root.right))+1;
+	}
+	static int size(Node root) {
+		if(root== null) return 0;
+		return size(root.left)+size(root.right)+1;
+	}
+	static int maximum(Node root) {
+		if (root == null) return Integer.MIN_VALUE;
+		return Math.max(root.data, Math.max(maximum(root.left), maximum(root.right)));
+	}
+	static int minimum(Node root) {
+		if (root == null) return Integer.MAX_VALUE;
+		return Math.min(root.data, Math.min(minimum(root.left), minimum(root.right)));
 	}
 }
 
